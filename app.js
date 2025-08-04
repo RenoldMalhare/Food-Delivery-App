cc59b69d7155e021851a1d809ffab2465dbaa7db
RenoldMalhare <renoldmalhare2@gmail.com>
2025-08-04 23:00:53 +0530

    food del

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const resObj = [
   {
     id: 2,
     name: "Bella Napoli",
-    image_url: "https://images.unsplash.com/photo-1606755962771-3bbf9484baf4",
+    image_url: "https://images.unsplash.com/photo-1513104890138-7c749659a591",
     cost_for_two: "$40",
     rating: 4.2,
     cuisines: ["Italian", "Pizza", "Pasta"],
@@ -45,7 +45,8 @@ const resObj = [
   {
     id: 3,
     name: "Tokyo Bites",
-    image_url: "https://images.unsplash.com/photo-1579583765283-52b3e4f11884",
+    image_url:
+      "https://plus.unsplash.com/premium_photo-1723874570807-570c56b41e4e",
     cost_for_two: "$35",
     rating: 4.8,
     cuisines: ["Japanese", "Sushi"],
@@ -72,7 +73,8 @@ const resObj = [
   {
     id: 6,
     name: "Taco Loco",
-    image_url: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4",
+    image_url:
+      "https://plus.unsplash.com/premium_photo-1679231521293-e13146411fb2",
     cost_for_two: "$22",
     rating: 4.0,
     cuisines: ["Mexican", "Tacos"],
@@ -81,7 +83,7 @@ const resObj = [
   {
     id: 7,
     name: "Falafel House",
-    image_url: "https://images.unsplash.com/photo-1552332386-f8dd00dc2f85",
+    image_url: "https://images.unsplash.com/photo-1680990999782-ba7fe26e4d0b",
     cost_for_two: "$30",
     rating: 4.3,
     cuisines: ["Middle Eastern", "Falafel"],
@@ -90,7 +92,7 @@ const resObj = [
   {
     id: 8,
     name: "Pho Street",
-    image_url: "https://images.unsplash.com/photo-1586190848861-99aa4a171e90",
+    image_url: "https://images.unsplash.com/photo-1503764654157-72d979d9af2f",
     cost_for_two: "$18",
     rating: 4.4,
     cuisines: ["Vietnamese", "Pho"],
@@ -99,7 +101,7 @@ const resObj = [
   {
     id: 9,
     name: "Café Aurora",
-    image_url: "https://images.unsplash.com/photo-1605478544042-290f2c7f1f5a",
+    image_url: "https://images.unsplash.com/photo-1579712267685-42da80f60aa4",
     cost_for_two: "$50",
     rating: 4.7,
     cuisines: ["French", "Fine Dining"],
@@ -108,7 +110,8 @@ const resObj = [
   {
     id: 10,
     name: "Dragon Wok",
-    image_url: "https://images.unsplash.com/photo-1603570419960-9b9cfb4e2f74",
+    image_url:
+      "https://plus.unsplash.com/premium_photo-1661600135596-dcb910b05340",
     cost_for_two: "$26",
     rating: 4.2,
     cuisines: ["Chinese", "Dim Sum"],
